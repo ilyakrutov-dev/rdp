61680ccf98703d27792755ca384bc540c5d3ae21
Ilya Krutov <45107158+BROILYA091@users.noreply.github.com>
2024-04-27 22:46:37 +0300

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-# Обновить систему
-sudo apt update && sudo apt upgrade -y
-
-# Установить среду GNOME
-sudo apt install ubuntu-desktop -y
-
-# Установить TightVNCServer
-sudo apt install tightvncserver -y
+sudo apt-get update && sudo apt-get install -y kubuntu-desktop tightvncserver
 
 vncserver << EOF
 q1qq1q
@@ -15,12 +6,3 @@ q1qq1q
 n
 EOF
 echo OK
-
-# Остановить VNC-сервер
-vncserver -kill :1
-
-# Запустить VNC-сервер с использованием среды GNOME
-vncserver -geometry 1920x1080 :1 -depth 24 -rfbport 5901
-
-# Вывести IP-адрес и номер порта VNC-сервера
-echo "VNC server is running on IP address: $(hostname -I) and port: 5901"
